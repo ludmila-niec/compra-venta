8810c34455ff6071f2e7521707f53e11ba35b204
Mahmoud Miz <mahmoudmaiz@icloud.com>
2020-08-13 02:57:47 -0300

    agregar la ruta para comprar un producto, agregar el nuevo producto para el comprador

--- DB/database.js
+++ DB/database.js
@@ -5,34 +5,66 @@ let usuarios = [];
 let productos = [];
 
 function agregarUsuario(usuario) {
-    //agrego array de productos creados por el usuario
-    usuario.misProductos = []
-    //agrego lista de compras
-    usuario.misCompras = []
-    usuario.id = idUsuario;
-    usuarios.push(usuario);
-    idUsuario++;
-    return usuario;
+  //agrego array de productos creados por el usuario
+  usuario.misProductos = [];
+  //agrego lista de compras
+  usuario.misCompras = [];
+  usuario.id = idUsuario;
+  usuarios.push(usuario);
+  idUsuario++;
+  return usuario;
 }
 
 function agregarProducto(producto, idUser) {
-    producto.id = idProducto;
-    producto.idUsuario = idUser;
-    //Ahora se agrega el producto al usuario correspondiente en su lista de 'misProductos'
-    let buscarUsuarioporId = usuarios.find((user) => user.id == idUser);
-    if (!buscarUsuarioporId) {
-        throw new Error ('No se encontró el id del vendedor')
+  producto.id = idProducto;
+  producto.idUsuario = idUser;
+  //Ahora se agrega el producto al usuario correspondiente en su lista de 'misProductos'
+  let buscarUsuarioporId = usuarios.find((user) => user.id == idUser);
+  if (!buscarUsuarioporId) {
+    throw new Error("No se encontró el id del vendedor");
+  }
+  console.log(buscarUsuarioporId);
+  buscarUsuarioporId.misProductos.push(producto);
+  productos.push(producto);
+  idProducto++;
+  return producto;
+}
+
+function realizarUnaCompra(idProducto, idUser) {
+  try {
+    // check si el producto existe
+    const producto = productos.find((p) => p.id == idProducto);
+    if (!producto) throw new Error(" no existe este producto ");
+
+    //agregue el producto al comprador
+    usuarios.forEach((usuario) => {
+      if (usuario.id == idUser) {
+        usuario.misCompras.push(producto);
+      }
+    });
+
+    // eliminar el producto de los productos del vendedor
+    for (let usuario of usuarios) {
+      if (usuario.id == producto.idUsuario) {
+        usuario.misProductos = usuario.misProductos.filter(
+          (p) => p.id != producto.id
+        );
+      }
     }
-    console.log(buscarUsuarioporId)
-    buscarUsuarioporId.misProductos.push(producto);
-    productos.push(producto);
-    idProducto++;
-    return producto;
+
+    // eliminar el producto de los productos
+    productos = productos.filter((p) => p.id != idProducto);
+
+    return true;
+  } catch (err) {
+    throw new Error(err.message);
+  }
 }
 
 module.exports = {
-    usuarios: usuarios,
-    productos: productos,
-    agregarUsuario: agregarUsuario,
-    agregarProducto: agregarProducto,
+  usuarios: usuarios,
+  productos: productos,
+  agregarUsuario: agregarUsuario,
+  agregarProducto: agregarProducto,
+  realizarUnaCompra: realizarUnaCompra,
 };
--- routes/product-route.js
+++ routes/product-route.js
@@ -2,37 +2,48 @@ const express = require("express");
 const router = express.Router();
 const productoServicio = require("../services/productos-service");
 const { usuarioAutorizado } = require("../middleware/auth");
+const { usuarios, realizarUnaCompra } = require("../db/database");
 
 //Crear productos
 router.post("/", usuarioAutorizado, (req, res) => {
-    try {
-        let nuevoProducto = req.body;
-        let idUsuario = req.usuario.id;
-        const { nombreProducto, precio, estado, descripcion } = nuevoProducto;
-        // falta validacion inputs completos
-        if (!(nuevoProducto && idUsuario)) {
-           return  res.status(400).json({
-                Error: "Faltan datos para crear el producto",
-            });
-        } else {
-            let resultado = productoServicio.crearProducto(
-                nuevoProducto,
-                idUsuario
-            );
-            if (resultado) {
-                res.status(201).json(resultado);
-            } else {
-                res.status(400).json({ Error: "No se pudo crear el producto" });
-            }
-        }
-    } catch (err) {
-        res.status(400).json({ Error: err.message });
+  try {
+    let nuevoProducto = req.body;
+    let idUsuario = req.usuario.id;
+    const { nombreProducto, precio, estado, descripcion } = nuevoProducto;
+    // falta validacion inputs completos
+    if (!(nuevoProducto && idUsuario)) {
+      return res.status(400).json({
+        Error: "Faltan datos para crear el producto",
+      });
+    } else {
+      let resultado = productoServicio.crearProducto(nuevoProducto, idUsuario);
+      if (resultado) {
+        res.status(201).json(resultado);
+      } else {
+        res.status(400).json({ Error: "No se pudo crear el producto" });
+      }
     }
+  } catch (err) {
+    res.status(400).json({ Error: err.message });
+  }
 });
 
 // retornar todos los productos
 router.get("/", (req, res) => {
-    let productos = productoServicio.listarProductos();
-    res.json(productos);
+  let productos = productoServicio.listarProductos();
+  res.json(productos);
+});
+
+router.post("/:idProducto", usuarioAutorizado, (req, res) => {
+  try {
+    let idUsuario = req.usuario.id;
+    const idProducto = req.params.idProducto;
+    let confirmaDeCompra = realizarUnaCompra(idProducto, idUsuario);
+    if (confirmaDeCompra) {
+      res.json(usuarios);
+    }
+  } catch (error) {
+    res.send(error.message);
+  }
 });
 module.exports = router;
